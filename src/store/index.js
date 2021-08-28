c1b21eda5a759f766d6b032868d23fb3964f89b3
Richard Chen <i@iochen.com>
2021-08-28 18:07:04 +0800

    new getters for token
    Signed-off-by: Richard Chen <i@iochen.com>

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+  },
+  getters: {
+    token: () => {
+      return Vue.$cookies.get('token')
+    }
   },
   mutations: {
   },
